fcfddc46bd318fc2e56c5ffd78530a810bbeffc0
Kinokov Shotaskovich <t4keda6osci01a@gmail.com>
2026-06-20 23:53:03 +0900

    シンボル名をより分かりやすく修正

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
-use std::str::FromStr;
 use std::{env, process::Command};
 
 use const_format::concatcp;
@@ -12,8 +11,7 @@ const ASSETS: &str = concatcp!(env!("CARGO_MANIFEST_DIR"), "/assets");
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let out_dir: &str = out_dir.to_str().unwrap();
-    let out_path = PathBuf::from_str(out_dir).unwrap();
+    let out_path = PathBuf::from(out_dir);
 
     // 画像の変換
     const IMAGE_PATH: &str = concatcp!(ASSETS, "/ferris.png");
@@ -24,28 +22,43 @@ fn main() {
 
     assert!(width.is_power_of_two() && height.is_power_of_two());
 
-    let img_view = ImageView::new(
-        img.as_bytes(),
-        width as usize,
-        height as usize,
-        ImageFormat::Rgba8,
-    );
-
-    let swizzled_pixels = swizzle_3ds::swizzle_image(&img_view);
-
+    // ミップマップを作成
+    let mipmaps_count = width.ilog2().max(height.ilog2());
+    let mipmaps = (0..=mipmaps_count)
+        .flat_map(|level| {
+            // 仕様上はオリジナルのサイズから1x1になるまでミップマップを作らないといけないが、
+            // 3DSのGPUが読み込む1ブロックは8x8が最小
+            let nwidth = (width >> level).max(8);
+            let nheight = (height >> level).max(8);
+            let resized = img.resize(nwidth, nheight, image::imageops::FilterType::Nearest);
+            let img_view = ImageView::new(
+                resized.as_bytes(),
+                nwidth as usize,
+                nheight as usize,
+                ImageFormat::Rgba8,
+            );
+            // Swizzle処理を適用
+            let swizzled_pixels = swizzle_3ds::swizzle_image(&img_view);
+            swizzled_pixels.as_raw().to_vec()
+        })
+        .collect::<Vec<u8>>();
     let dest_path = out_path.join("swizzled_ferris.bin");
     let mut f = File::create(&dest_path).unwrap();
-
-    f.write_all(swizzled_pixels.as_raw()).unwrap();
+    f.write_all(&mipmaps).unwrap();
 
     // シェーダのコンパイル
     const SHADER_PATH: &str = concatcp!(ASSETS, "/sprite.v.pica");
     println!("cargo:rerun-if-changed={}", SHADER_PATH);
 
-    Command::new("picasso")
+    let status = Command::new("picasso")
         .arg(SHADER_PATH)
         .arg("-o")
         .arg(out_path.join("sprite.shbin"))
         .status()
         .unwrap();
+
+    assert!(
+        status.success(),
+        "Failed to compile the shader with picasso"
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -32,45 +32,50 @@ impl Vertex {
     }
 }
 
-fn create_texture(image: &[u8]) -> (Texture, Info) {
+fn create_texture(image: &[u8], img_size: (u16, u16)) -> Texture {
     // テクスチャの生成
     let face = Face::default();
     let mut texture = Texture::new(TextureParameters::new_2d(
-        IMAGE_X_SIZE,
-        IMAGE_Y_SIZE,
+        img_size.0,
+        img_size.1,
         citro3d::texture::ColorFormat::Rgba8,
     ))
     .unwrap();
     texture.load_image(image, face).unwrap();
 
+    texture
+}
+
+fn create_3d_model(disp_size: (f32, f32)) -> Info {
     // 3Dモデルを定義
     const Z: f32 = 0.;
-    const VERTEXS: [Vertex; 4] = [
+    let vertexs: [Vertex; 4] = [
         Vertex {
             pos: [0.0, 0.0, Z],
             uv: [0.0, 0.0],
         },
         Vertex {
-            pos: [DISP_SIZE, 0.0, Z],
+            pos: [disp_size.0, 0.0, Z],
             uv: [1.0, 0.0],
         },
         Vertex {
-            pos: [0.0, DISP_SIZE, Z],
+            pos: [0.0, disp_size.1, Z],
             uv: [0.0, 1.0],
         },
         Vertex {
-            pos: [DISP_SIZE, DISP_SIZE, Z],
+            pos: [disp_size.0, disp_size.1, Z],
             uv: [1.0, 1.0],
         },
     ];
 
-    let buff = Buffer::new(&VERTEXS);
+    // LinearMemoryに確保したメモリ上に頂点座標をコピー
+    let buff = Buffer::new(&vertexs);
     let mut info = citro3d::buffer::Info::default();
 
     info.add(buff.clone(), Vertex::layout_permutation())
         .unwrap();
 
-    (texture, info)
+    info
 }
 
 fn render_frame(
@@ -80,9 +85,9 @@ fn render_frame(
     target: &mut ScreenTarget<'static>,
     citro: &mut Instance,
     program: &Program,
-    projection_idx: Index,
+    pm_idx: Index,
 ) {
-    // クリップ平面を定義
+    // プロジェクション行列を定義
     let projection_matrix: citro3d::math::Matrix4 = citro3d::math::Projection::orthographic(
         0.0..SCREEN_HEIGHT as f32,
         0.0..SCREEN_WIDTH as f32,
@@ -109,7 +114,7 @@ fn render_frame(
         )
         .unwrap();
 
-    let stage0 = citro3d::texenv::TexEnv::new()
+    let texenv = citro3d::texenv::TexEnv::new()
         .src(
             citro3d::texenv::Mode::BOTH,
             citro3d::texenv::Source::Texture0,
@@ -124,12 +129,11 @@ fn render_frame(
     // 画面クリア
     target.clear(citro3d::render::ClearFlags::ALL, 0xFF000000, 0);
 
-    citro.render_frame_with(|mut instance| {
-        instance.select_render_target(target).unwrap();
-        instance.set_attr_info(&attr_info);
-
-        instance.bind_program(&program);
-        instance.set_texenvs(&[stage0.clone()]);
+    citro.render_frame_with(|mut frame| {
+        frame.select_render_target(target).unwrap();
+        frame.set_attr_info(&attr_info);
+        frame.bind_program(&program);
+        frame.set_texenvs(&[texenv.clone()]);
 
         let mut matrix = projection_matrix;
         unsafe {
@@ -137,20 +141,19 @@ fn render_frame(
             citro3d_sys::Mtx_Translate(matrix.as_raw_mut(), coord.0, coord.1, coord.2, true);
         }
 
-        instance.bind_vertex_uniform(projection_idx, &matrix);
-        instance.bind_texture(citro3d::texture::Index::Texture0, texture);
+        frame.bind_vertex_uniform(pm_idx, &matrix);
+        frame.bind_texture(citro3d::texture::Index::Texture0, texture);
 
-        instance
+        frame
             .draw_arrays(citro3d::buffer::Primitive::TriangleStrip, info, None)
             .unwrap();
 
-        instance
+        frame
     });
 }
 
 fn main() {
     let apt = Apt::new().unwrap();
-    let mut hid = Hid::new().unwrap();
     let gfx: &'static Gfx = Box::leak(Box::new(Gfx::new().unwrap()));
     let mut citro: Instance = citro3d::Instance::new().unwrap();
 
@@ -161,9 +164,9 @@ fn main() {
     let binary = library.get(0).unwrap();
     let program = Program::new(binary).expect("Failed to create shader program");
 
-    let mvp_uniform_idx = program
-        .get_vertex_uniform("mvp")
-        .expect("Uniform 'mvp' not found in shader");
+    let pm_uniform_idx = program
+        .get_vertex_uniform("pm")
+        .expect("Uniform 'pm' not found in shader");
 
     // スクリーンターゲットの構築
     let top_screen = gfx.top_screen.borrow_mut();
@@ -171,20 +174,19 @@ fn main() {
         .render_target(SCREEN_WIDTH, SCREEN_HEIGHT, top_screen, None)
         .unwrap();
 
-    // テクスチャを構築
-    let image = include_bytes!(concat!(env!("OUT_DIR"), "/swizzled_ferris.bin"))
-        .to_vec()
-        .leak();
-    let (texture, info) = create_texture(image);
+    // テクスチャと3Dモデルを構築
+    let image = include_bytes!(concat!(env!("OUT_DIR"), "/swizzled_ferris.bin"));
+    let texture = create_texture(image, (IMAGE_X_SIZE, IMAGE_Y_SIZE));
+    let model = create_3d_model((DISP_SIZE, DISP_SIZE));
 
-    let instanct = Instant::now();
+    let instant = Instant::now();
     while apt.main_loop() {
         gfx.wait_for_vblank();
 
         // Ferrisくんを往復させる
         const SECS_PER_ROUND_TRIP: f32 = 4.;
         let progress: f32 =
-            (instanct.elapsed().as_secs_f32() % SECS_PER_ROUND_TRIP) / SECS_PER_ROUND_TRIP;
+            (instant.elapsed().as_secs_f32() % SECS_PER_ROUND_TRIP) / SECS_PER_ROUND_TRIP;
         let x = if progress < 0.5 {
             // 右に進んでいるときの座標計算
             let right_prog = progress * 2.;
@@ -197,17 +199,12 @@ fn main() {
 
         render_frame(
             &texture,
-            &info,
+            &model,
             (x, 0., 0.),
             &mut target,
             &mut citro,
             &program,
-            mvp_uniform_idx,
+            pm_uniform_idx,
         );
-
-        hid.scan_input();
-        if hid.keys_down().contains(KeyPad::START) {
-            break;
-        }
     }
 }
